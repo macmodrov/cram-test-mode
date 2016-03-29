ef6b1fd42dab1bbdd6f2240ce08c07489cc2d4ec
macmodrov <em3@minions.org.pl>
2016-03-29 18:02:35 +0200

    Fixed global removal of font-lock-string-face.
    Made variable buffer-local.

--- cram-test-mode.el
+++ cram-test-mode.el
@@ -47,7 +47,7 @@
 (define-derived-mode cram-test-mode fundamental-mode "Cram test"
   "Major mode for Cram tests highlighting."
   (setq font-lock-defaults cram-test-font-lock-defaults)
-  (setq font-lock-string-face nil)
+  (setq-local font-lock-string-face nil)
   (set (make-local-variable 'tab-width) 2)
 )
 
